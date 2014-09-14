c6c1199dea324f5a3b65bba95decba4c80ecd732
minami1389 <minami@baba-no-MacBook-Air.local>
2014-09-15 00:07:43 +0900

    Initial Commit

---
+++ MealReport/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  MealReport
+//
+//  Created by minami on 2014/09/15.
+//  Copyright (c) 2014年 Minami Baba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
---
+++ MealReport/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MealReport
+//
+//  Created by minami on 2014/09/15.
+//  Copyright (c) 2014年 Minami Baba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMAppDelegate class]));
+    }
+}
