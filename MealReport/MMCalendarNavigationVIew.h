04ef5cb4db98b2e8787b0135279097b998913763
minami <s1311389@coins.tsukuba.ac.jp>
2015-05-15 14:23:07 +0900

    一時push

---
+++ MealReport/MMCalendarNavigationVIew.h
@@ -0,0 +1,13 @@
+//
+//  MMCalendarNavigationVIew.h
+//  MealReport
+//
+//  Created by minami on 4/30/15.
+//  Copyright (c) 2015 Minami Baba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMCalendarNavigationVIew : UIView
+
+@end
---
+++ MealReport/MMCalendarNavigationVIew.m
@@ -0,0 +1,21 @@
+//
+//  MMCalendarNavigationVIew.m
+//  MealReport
+//
+//  Created by minami on 4/30/15.
+//  Copyright (c) 2015 Minami Baba. All rights reserved.
+//
+
+#import "MMCalendarNavigationVIew.h"
+
+@implementation MMCalendarNavigationVIew
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
